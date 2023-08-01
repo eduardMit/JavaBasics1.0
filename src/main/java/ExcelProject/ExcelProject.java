d4f56d7977638f5f833a53058dc95ac4f8d258ae
eduardMit <eduard.mitrica98@yahoo.com>
2023-08-01 20:09:04 +0300

    8th Commit

--- src/main/java/ExcelProject/ExcelProject.java
+++ src/main/java/ExcelProject/ExcelProject.java
@@ -1,47 +1,42 @@
-package Streams;
+package ExcelProject;
 
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Scanner;
 
-public class ExcelProject{
-
+public class SearchInExcelFiles {
     public static void main(String[] args) {
-        String folderPath = "C:\\Users\\eduar\\Downloads\\data.csv";
-        Scanner scan1 = new Scanner(System.in);
-        System.out.println("Please enter the the word you are looking for: ");
-        String searchText = scan1.nextLine();
+        String searchWord = "cuvântul";
+        String folderPath = "C:\\Users\\eduar\\Downloads\\data.xlms";
 
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
+
         if (files != null) {
             for (File file : files) {
                 if (file.isFile() && (file.getName().endsWith(".xlsx") || file.getName().endsWith(".xlsm"))) {
-                    FileInExcel(file, String.valueOf(searchText));
+                    searchInExcelFile(file, searchWord);
                 }
             }
+        } else {
+            System.out.println("Folderul nu există sau nu conține fișiere.");
         }
     }
 
-    private static void FileInExcel(File file, String searchText) {
+    private static void searchInExcelFile(File file, String searchWord) {
         try (FileInputStream fis = new FileInputStream(file);
-             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+             Workbook workbook = WorkbookFactory.create(fis)) {
 
-            DataFormatter dataFormatter = new DataFormatter();
             for (Sheet sheet : workbook) {
                 for (Row row : sheet) {
                     for (Cell cell : row) {
-                        String cellValue = dataFormatter.formatCellValue(cell);
-                        if (cellValue.contains(searchText)) {
-                            System.out.println("Found in file: " + file.getName());
-                            System.out.println("Sheet: " + sheet.getSheetName());
-                            System.out.println("Row: " + (row.getRowNum() + 1));
-                            System.out.println("Cell: " + cell.getAddress());
-                            System.out.println();
+                        if (cell.getCellType() == CellType.STRING) {
+                            String cellValue = cell.getStringCellValue();
+                            if (cellValue.contains(searchWord)) {
+                                printSearchResult(file.getName(), sheet.getSheetName(), row.getRowNum() + 1);
+                            }
                         }
                     }
                 }
@@ -50,4 +45,12 @@ public class ExcelProject{
             e.printStackTrace();
         }
     }
+
+    private static void printSearchResult(String fileName, String sheetName, int rowNum) {
+        System.out.println("Găsit în fișier: " + fileName);
+        System.out.println("Foaie: " + sheetName);
+        System.out.println("Rând: " + rowNum);
+        System.out.println();
+    }
 }
+
